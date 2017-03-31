ee2a4ad69a143af833003d5e415f9309cec58428
Kijong Han <khan465@aucklanduni.ac.nz>
2017-03-31 17:59:27 +1300

    Migrated date implementation to database

--- Data/Migrations/20170331045658_added-creation-times.Designer.cs
+++ Data/Migrations/20170331045658_added-creation-times.Designer.cs
@@ -8,9 +8,10 @@ using AvaNet.Data;
 namespace AvaNet.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170331045658_added-creation-times")]
+    partial class addedcreationtimes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.1")
