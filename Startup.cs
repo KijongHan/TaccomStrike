2ce5d429ccea961ceefd266f07b6baf780107596
Kijong Han <khan465@aucklanduni.ac.nz>
2017-04-11 15:57:47 +1200

    changed database to use psql

--- Startup.cs
+++ Startup.cs
@@ -52,7 +52,8 @@ namespace AvaNet
             services.AddApplicationInsightsTelemetry(Configuration);
 
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+                options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+                //options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
