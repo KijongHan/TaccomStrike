e4630912396487773e44a9ac6f9041af9fad7531
Kijong Han <khan465@aucklanduni.ac.nz>
2018-09-22 01:57:02 +1200

    Major refactoring. Consolidate anonymous types to static types, Remove unused logic, consolidate logic, Add game library, Add api view models, Simplify hub connections

---
+++ TaccomStrike.Web.UI/dist/viewmodels/chatmessage.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ChatMessageViewModel = /** @class */ (function () {
+    function ChatMessageViewModel() {
+    }
+    return ChatMessageViewModel;
+}());
+exports.ChatMessageViewModel = ChatMessageViewModel;
+//# sourceMappingURL=chatmessage.js.map
---
+++ TaccomStrike.Web.UI/dist/viewmodels/chatroom.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ChatRoomViewModel = /** @class */ (function () {
+    function ChatRoomViewModel() {
+    }
+    return ChatRoomViewModel;
+}());
+exports.ChatRoomViewModel = ChatRoomViewModel;
+//# sourceMappingURL=chatroom.js.map
