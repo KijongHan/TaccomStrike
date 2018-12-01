add268948e56854d96a6f2252f967cbd235050a3
Kijong Han <khan465@aucklanduni.ac.nz>
2018-12-01 14:52:46 +1300

    - Add vscode launch configuration - Remove app.css css dependency - Add development mode to webpack - Remove redundant script references in index.html file

--- TaccomStrike.Web.UI/src/index.tsx
+++ TaccomStrike.Web.UI/src/index.tsx
@@ -2,8 +2,6 @@
 import * as ReactDOM from "react-dom";
 import { LoginPageComponent } from "./components/page/login";
 
-import '../styles/app.css';
-
 ReactDOM.render(
 	<LoginPageComponent/>,
 	document.getElementById("root")
