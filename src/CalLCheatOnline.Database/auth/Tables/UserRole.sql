4a33718d8eee164e5924973e63d9c6e6d7b6d4bc
Kijong Han <khan465@aucklanduni.ac.nz>
2019-07-23 18:55:28 +1200

    Remove unnecessary Database project. Update project structure and update solution file

--- src/CalLCheatOnline.Database/auth/Tables/UserRole.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE [auth].[UserRole] 
-(
-    [UserRoleID] INT IDENTITY (1,1) NOT NULL,
-    [RoleName] NVARCHAR(100) NOT NULL,
-    [WhenCreated] DATETIME,
-    [WhenDeleted] DATETIME,
-    CONSTRAINT [PK_UserRole] PRIMARY KEY CLUSTERED ([UserRoleID] ASC)
-);
-GO
--- src/CalLCheatOnline.Database/game/Tables/GameUser.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [game].[GameUser] 
-(
-    [GameUserID] INT IDENTITY(1,1) NOT NULL,
-	[GameScore] INT NOT NULL,
-    [WhenCreated] DATETIME,
-    [WhenDeleted] DATETIME,
-    CONSTRAINT [PK_GameUser] PRIMARY KEY CLUSTERED ([GameUserID])
-);
