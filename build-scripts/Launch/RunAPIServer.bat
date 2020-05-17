69bb1eae86d524b7fe0281586be025838bf7d475
Thomas Han <eyeseemint@gmail.com>
2020-05-17 18:45:00 +1200

    Fix build error

--- build-scripts/Launch/RunAPIServer.bat
+++ build-scripts/Launch/RunAPIServer.bat
@@ -7,6 +7,7 @@ echo "Deployment: Retrieving latest changes from git"
 git pull
 
 echo "Deployment: Running Configuration Manager"
+cd src
 dotnet run -p "CallCheatOnline.Console.ConfigurationManager" "Development" "Server=localhost,1433;Database=CallCheat_Production;User Id=application_login;Password=159789Qaz;"
 cd ..
 
