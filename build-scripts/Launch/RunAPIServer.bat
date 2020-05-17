7ab16c9169cb3cf07787f419e89b51e179764e8e
Thomas Han <eyeseemint@gmail.com>
2020-05-17 19:07:18 +1200

    Add fix where the build output is not available immediately

--- build-scripts/Launch/RunAPIServer.bat
+++ build-scripts/Launch/RunAPIServer.bat
@@ -9,12 +9,13 @@ git pull
 echo "Deployment: Running Configuration Manager"
 cd src
 dotnet run -p "CallCheatOnline.Console.ConfigurationManager" "Development" "Server=localhost,1433;Database=CallCheat_Production;User Id=application_login;Password=159789Qaz;"
-cd ..
 
 echo "Deployment: Running Web API Server"
 cd "CallCheatOnline.Web.API"
 dotnet publish --configuration Release
 
+echo "Moving server applicatino to public web folder"
 cp "bin/Release/netcoreapp2.2/publish/CallCheatOnline.Web.API.dll" "/var/www/callcheatonline/CallCheatOnline.Web.API.dll"
 
+echo "Starting api server service"
 sudo systemctl start callcheatonline_apiserver.service
